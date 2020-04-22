9ab448969fa5054f8f03150094df961040cf7170
gblend <gabrielilo190@gmail.com>
2020-04-22 14:55:23 +0100

    add sqlite db

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -8,15 +8,21 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-   id = db.column(db.Integer, primary_key=True)
-   content = db.column(db.String(250), nullable=False)
-   date_created = db.column(db.DateTime, default=datetime.utcnow)
+   id = db.Column(db.Integer, primary_key=True)
+   content = db.Column(db.String(250), nullable=False)
+   date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+   def __repr__(self):
+      return '<Task %r>' % self.id
 
    
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-   return render_template('index.html')
+   if request.method == 'POST':
+      return 'pass'
+   else:
+      return render_template('index.html')
 
 
 if __name__ == '__main__':
